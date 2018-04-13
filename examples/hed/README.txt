01b7c4701cd3e18310cef4970ccc9089ccd7216d
hzwangjialei <hzwangjailei@corp.netease.com>
2018-04-13 14:52:21 +0800

    更新训练相关参数

---
+++ examples/hed/README.txt
@@ -0,0 +1,3 @@
+1.将标注好的文件夹放到 hed/data/下面，使用create_list.py 创建lst文件
+2.指定train_val.prototxt中训练数据路径
+
--- examples/hed/train.sh
+++ examples/hed/train.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+rm train.log -rf
+rm training_data -rf
+
+mkdir training_data
 
 python solve.py 2>&1| tee train.log &
